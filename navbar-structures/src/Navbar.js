4c9b0fec723fedfbe0acd323357a3a19a9bda41f
BelenMaru <bleneee@gmail.com>
2022-02-02 20:39:41 -0500

    add social to the nav

--- navbar-structures/src/Navbar.js
+++ navbar-structures/src/Navbar.js
@@ -1,20 +1,20 @@
-import react from 'react';
-import logo from './logo.svg';
+import react from "react";
+import { FaBars } from "react-icons/fa";
+import logo from "./logo.svg";
 
 const Navbar = () => {
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
-        <img src={logo} alt ="logo" />
-        <button className="nav-toggle">
-          
-        </button>
+          <img src={logo} alt="logo" />
+          <button className="nav-toggle">
+            <FaBars />
+          </button>
+        </div>
       </div>
-      </div>
-
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
--- navbar-structures/src/data.js
+++ navbar-structures/src/data.js
@@ -27,3 +27,27 @@ export const links = [
     text: "profile",
   },
 ];
+
+
+export const social = [
+  {
+    id: 1,
+    url: "https://www.twitter.com",
+    icon: <FaFacebook />,
+  },
+  {
+    id: 2,
+    url: "https://www.twitter.com",
+    icon: <FaTwitter />,
+  },
+  {
+    id: 3,
+    url: "https://www.twitter.com",
+    icon: <FaLinkedin />,
+  },
+  {
+    id: 4,
+    url: "https://www.twitter.com",
+    icon: <FaBehance />,
+  },
+];
